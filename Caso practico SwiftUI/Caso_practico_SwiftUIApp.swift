86e6ea5179c4f02a9ae9460a6722d80c88a896db
Luis Humberto <beto020017@gmail.com>
2022-07-16 13:32:45 -0500

    Initial Commit

---
+++ Caso practico SwiftUI/Caso_practico_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Caso_practico_SwiftUIApp.swift
+//  Caso practico SwiftUI
+//
+//  Created by Luis Humberto Martinez Echegaray on 16/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Caso_practico_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
